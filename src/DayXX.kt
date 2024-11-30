a35e688c339ad463de2f1b5d33125554a7de64ae
Marek Kohn <me@makohn.de>
2024-11-30 19:09:56 +0100

    Add template

---
+++ src/DayXX.kt
@@ -0,0 +1,21 @@
+
+fun main() {
+
+    val day = "<DAY>"
+
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day${day}_test")
+    check(part1(testInput) == 0)
+
+    val input = readInput("Day${day}")
+    println(part1(input))
+    println(part2(input))
+}
