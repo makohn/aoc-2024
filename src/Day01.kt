ecc86b2dc7244ae965b432a6e2642af3ae6d1eb1
Marek Kohn <me@makohn.de>
2024-12-01 06:14:31 +0100

    Add solution for day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,52 @@
+
 fun main() {
+
+    val day = "01"
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val list1 = mutableListOf<Int>()
+        val list2 = mutableListOf<Int>()
+        for (line in input) {
+            val split = line.split("   ")
+            val a = split[0]
+            val b = split[1]
+            list1.add(a.toInt())
+            list2.add(b.toInt())
+        }
+        list1.sort()
+        list2.sort()
+        var acc = 0
+        for (i in 0..<list1.size) {
+            acc += Math.abs(list1[i] - list2[i])
+        }
+        return acc
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val list1 = mutableListOf<Int>()
+        val list2 = mutableListOf<Int>()
+        for (line in input) {
+            val split = line.split("   ")
+            val a = split[0]
+            val b = split[1]
+            list1.add(a.toInt())
+            list2.add(b.toInt())
+        }
+
+        var acc = 0
+        for (i in 0..< list1.size) {
+            val count = list2.count { it == list1[i] }
+            acc += list1[i] * count
+        }
+        return acc
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day${day}_test")
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    val input = readInput("Day${day}")
+    println(part1(input))
+    println(part2(input))
 }
