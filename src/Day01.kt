d7d24d485da68c17b1cbe9dc25ca883baac23fc0
Marek Kohn <me@makohn.de>
2024-12-01 14:06:49 +0100

    Refactor solution for day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,47 +1,24 @@
+import kotlin.math.abs
 
 fun main() {
 
     val day = "01"
 
     fun part1(input: List<String>): Int {
-        val list1 = mutableListOf<Int>()
-        val list2 = mutableListOf<Int>()
-        for (line in input) {
-            val split = line.split("   ")
-            val a = split[0]
-            val b = split[1]
-            list1.add(a.toInt())
-            list2.add(b.toInt())
-        }
-        list1.sort()
-        list2.sort()
-        var acc = 0
-        for (i in 0..<list1.size) {
-            acc += Math.abs(list1[i] - list2[i])
-        }
-        return acc
+        val lists = input
+            .map { it.split(Regex("\\s+")).map { it.toInt() } }
+            .let { lists -> lists.map { it[0] } to lists.map { it[1] } }
+        return lists.first.sorted().zip(lists.second.sorted())
+            .sumOf { pair -> abs(pair.first - pair.second) }
     }
 
     fun part2(input: List<String>): Int {
-        val list1 = mutableListOf<Int>()
-        val list2 = mutableListOf<Int>()
-        for (line in input) {
-            val split = line.split("   ")
-            val a = split[0]
-            val b = split[1]
-            list1.add(a.toInt())
-            list2.add(b.toInt())
-        }
-
-        var acc = 0
-        for (i in 0..< list1.size) {
-            val count = list2.count { it == list1[i] }
-            acc += list1[i] * count
-        }
-        return acc
+        val lists = input
+            .map { it.split(Regex("\\s+")).map { it.toInt() } }
+            .let { lists -> lists.map { it[0] } to lists.map { it[1] } }
+        return lists.first.sumOf { it * lists.second.count { i -> i == it }  }
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day${day}_test")
     check(part1(testInput) == 11)
     check(part2(testInput) == 31)
